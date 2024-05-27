62ef0124d18148ce3c3a55da0e10dc1e33df2e4c
frondendasoslar <faxriddin19951021@gmai.com>
2024-05-27 18:00:55 +0500

    malumot kiritish, consolda chiqarish, malumotni o'zgaruvchiga biriktirish

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* Javascriptda matnlar matnlar bittalik yoki ikkitalik qo'shtirnoq yoki quyidagi `` slesh ichiga yoziladi sonlarni esa o'z holicha yozish mumkum
+Agar qo'shtirnoq qatnashgan matn berilgan bo'lsa Uni quyidagicha yozish mumkun 'Oga\'ayin'. Consolda Og'ayin matni chiqadi
+Matnlarni `` sesh ichiga yozish yangi versiyalarda amal qiladi va eng qulay usul hisoblanadi
+
+O'zgaruvchi qiymatlarini quyidagicha ko'rinishda boshqa o'zgaruvchiga biriktirib qo'yishimiz bizga kod yozish jarayonimizni ancha sodda lashtiradi. masalan biz bitta o'zgaruvchini 50 ta joyda ishlatgan bo'lsak u o'zgaruvchini yana o'zgartirishimizga to'g'ri keladigan bo'lsa 50 ta o'zgaruvchini hammasini emas biriktirilgan o'zgaruvchini o'zgartirib qo'ysak bo'ldi
+Masalan:
+
+const Ism = `Faxriddin`
+
+console.log(Ism);
+console.log(Ism);
+console.log(Ism);
+console.log(Ism);
+console.log(Ism);
+console.log(Ism);
+console.log(Ism);
+console.log(Ism);
+
+Agar yuqoridagi 8 ta qatorda o'zgaruvchini o'zgartirishimizga to'g'ri keladigan bo'lsa hammasini o'zgartirmasdan ism o'zgaruvchisiga biriktirilgan o'zgaruvchini o'zgartiribqo'ysak bo'ldi */
